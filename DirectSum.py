e718637349f4cb003eb0ec939ec584508e3c6f44
Ellis Williams <elliswilliams52706@gmail.com>
2025-11-11 17:17:50 -0500

    pseudocode

--- DirectSum.py
+++ DirectSum.py
@@ -7,3 +7,26 @@ Created on Tue Nov 11 16:37:26 2025
 """
 
 # applying the integrator with direct summation
+
+
+
+# call body.py
+
+# generate n bodies with initial positions and velocities
+
+# for each time step
+
+    # for each body
+    
+        # reset the force on this body
+    
+        # add the force between this body and all other bodies
+        
+        # update position and velocity (advancing 1 time step)
+        
+        # plot the body's new position
+        
+    
+    # show the plot of all bodies / add to movie
+    
+    # advance to next time step
