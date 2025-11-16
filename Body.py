70c0ed36d4e2bef021246fd8b6611b4d58fcf1b4
druchika-cloud <druchika@unc.edu>
2025-11-16 16:04:08 -0500

    Update Body.py

--- Body.py
+++ Body.py
@@ -17,22 +17,23 @@ class body:
     # initializing body object with position, velocity, mass, color
     def  __init__(self, rx, ry, vx, vy, mass, color):
         
+        # positions 
         self.rx = rx
         self.ry = ry
+        
+        # velocities 
         self.vx = vx
         self.vy = vy
+        
+        # mass and color
         self.mass = mass
         self.color = color
-    
-    # update method completes one time step
-    # UPDATE THIS FUNCTION TO RK4
-    def update(self,dt):
         
-        vx += dt * fx / mass
-        vy += dt * fy / mass
-        rx += dt * vx
-        ry += dt * vy
-    
+        # inital force accumulation 
+        self.fx = 0.0
+        self.fy = 0.0
+       
+        
     # distance_to calculates the distance between this body and other body
     def distance_to(self, other):
         
@@ -42,11 +43,13 @@ class body:
     
     # zeroing force between iterations
     def resetforce(self):
-        fx = 0.0
-        fy = 0.0
+        self.fx = 0.0
+        self.fy = 0.0
         
     # calculating the force between this body and other body
     def addforce(self,other):
+        
+        G = 6.673e-11
         # calculating distance between the particles
         dx = other.rx - self.rx
         dy = other.ry - self.ry
@@ -54,14 +57,40 @@ class body:
         # calculating gravitational force
         f = G * (self.mass * other.mass) / (dist**2)
         # adding this force to this body
-        self.fx += F * dx / dist
-        self.fy += F * dy / dist
+        
+        self.fx += f * dx / dist    # Fcos(theta) = f * dx/dist
+        self.fy += f * dy / dist  
+   
+        
+            
+    # update method completes one time step
+    def update(self,dt): 
+        
+        '''
+        vx += dt * fx / mass
+        vy += dt * fy / mass
+        rx += dt * vx
+        ry += dt * vy
+        return vx, vy, rx, ry
+        '''
+        # perhaps this works? 
+        ax = self.fx/self.mass  # calc acceleration from force/mass
+        ay = self.fy/self.mass 
+        
+        self.vx += ax*dt        # update velocity w/ acceleration and dt
+        self.vy += ay*dt 
+        
+        self.rx += self.vx *dt  # update positions w/ vdt 
+        self.ry += self.vy *dt 
+        
     
     # putting all of the number variables in a string
     def tostring(self):
-        return "" + rx + ", "+ ry + ", "+  vx + ", "+ vy + ", "+ mass
-    
+        return f"Body(rx={self.rx: .3f}, ry={self.ry:.3f}, vx={self.vx:.3f}, vy={self.vy:.3f}, m={self.mass:.3f})"
+
     # checking what quadrant this body is in
     def inquad(self,q):
         return q.contains(self.rx,self.ry)
+    
+
     
