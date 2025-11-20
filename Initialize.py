716ddd16a3d1650feb920aa4c27d685c638bc087
druchika-cloud <druchika@unc.edu>
2025-11-19 23:15:49 -0500

    Update Initialize.py

--- Initialize.py
+++ Initialize.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # call body.py
-from Body import body 
+from Body2 import body 
 
 # initializing the bodies in a normal distribution for testing
 def makebodies_test(N):
@@ -24,39 +24,21 @@ def makebodies_test(N):
     vrange = 0.0 # maximum velocity
     
     bodies = []
+    
+    rng = np.random.default_rng(seed=42)   # <-- moved outside the loop
 
-    for n in range(N): 
+    for n in range(N):
+        rx = rng.normal(0, 5)
+        ry = rng.normal(0, 5)
+        vx = rng.normal()
+        vy = rng.normal()
 
-        rx = np.random.normal(0,5)
-        ry = np.random.normal(0,5)
-        vx = np.random.normal()
-        vy = np.random.normal()
-        
         b = body(rx, ry, vx, vy, mass, color1)
         bodies.append(b)
+
         
     return bodies
 
-
-def makebodies_bigmass():
-    # generating one big body and one little body
-    
-    windowsize = 10 # AU
-    
-    b_big = body(0,0,0,0,100,"white")
-    b_little = body(0.7*windowsize,0.7*windowsize,0,0,1,"blue")
-    
-    bodies = [b_big,b_little]
-    return bodies
-
-
-def earth_test():
-    
-    windowsize = 1.5 # AU
-    sun = body(0,0,0,0,1,"white")
-    earth = body(1,0,0,2*np.pi,3e-6,"white")
-    bodies = [sun,earth]
-    return bodies
 
 # # defining universal animation protocall
 # def ani_setup():
